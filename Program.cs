bc73f531055a77a693b5083be59088f3493b9b76
Chelsea <chelseasingla123@gmail.com>
2024-02-04 11:16:03 +0530

    first commit by Chelsea

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+app.MapGet("hello", () => "Hello World");
+app.MapGet("helloobj", () => new{
+    Hello ="World is mine" // here Hello is variable or object 
+});
+
+app.MapGet("add/{num1}/{num2}" , (int num1, int num2) => $"Sum: {num1 + num2}");
+
+app.MapGet("hello/{name}", (string name) => $"Hello, {name}!");
+//app.MapGet();
+app.Run();
